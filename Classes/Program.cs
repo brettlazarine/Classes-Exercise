62c7862cd82bf13b50d6d2c1232bdcb664e52e41
Brett Lazarine <jblazarine@gmail.com>
2023-02-11 07:03:30 -0600

    complete exercise 1

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Classes
+﻿using Microsoft.VisualBasic;
+using System.Xml.Schema;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var f150 = new Car()
+            {
+                Make = "Ford",
+                Model = "F150",
+                Year = 2015
+            };
+            Console.WriteLine($"Make: {f150.Make}\n" +
+                $"Model: {f150.Model}\n" +
+                $"Year: {f150.Year}");
         }
     }
 }
